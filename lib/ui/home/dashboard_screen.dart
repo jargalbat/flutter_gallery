ec4a436bdc5640590d053fe1aa40d09626f05f17
jargalbat <jargalbat@gmail.com>
2021-11-16 18:10:44 +0800

    sliver changes

--- lib/ui/home/dashboard_screen.dart
+++ lib/ui/home/dashboard_screen.dart
@@ -11,69 +11,54 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
-        _StickyHeaderList(index: 0),
-        _StickyHeaderList(index: 1),
-        _StickyHeaderList(index: 2),
-        _StickyHeaderList(index: 3),
+        // StickyHeader(index: 1),
+        // StickyHeader(index: 2),
+        // StickyHeader(index: 3),
+        _bottomAppBars(),
+        _bottomAppBars(),
+        _bottomAppBars(),
+        _bottomAppBars(),
       ],
       reverse: false,
     );
   }
-}
 
-
-class Header extends StatelessWidget {
-  const Header({
-    Key? key,
-    this.index,
-    this.title,
-    this.color = Colors.lightBlue,
-  }) : super(key: key);
-
-  final String? title;
-  final int? index;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      color: color,
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
-      alignment: Alignment.centerLeft,
-      child: Text(
-        title ?? 'Header #$index',
-        style: const TextStyle(color: Colors.white),
+  Widget _bottomAppBars() {
+    return SliverStickyHeader(
+      header: _sliverHeader('Bottom app bars'),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate(
+          [
+            Container(color: Colors.red, height: 150.0),
+            Container(color: Colors.purple, height: 150.0),
+            Container(color: Colors.green, height: 150.0),
+          ],
+        ),
+        // delegate: SliverChildBuilderDelegate(
+        //   (context, i) => ListTile(
+        //     leading: CircleAvatar(
+        //       child: Text('$i'),
+        //     ),
+        //     title: const Text('List tile'),
+        //     onTap: () {
+        //
+        //     },
+        //   ),
+        //   childCount: 6,
+        // ),
       ),
     );
   }
-}
 
-class _StickyHeaderList extends StatelessWidget {
-  const _StickyHeaderList({
-    Key? key,
-    this.index,
-  }) : super(key: key);
-
-  final int? index;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverStickyHeader(
-      header: Header(index: index),
-      sliver: SliverList(
-        delegate: SliverChildBuilderDelegate(
-          (context, i) => ListTile(
-            leading: CircleAvatar(
-              child: Text('$index'),
-            ),
-            title: Text('List tile #$i'),
-          ),
-          childCount: 6,
-        ),
-      ),
+  Widget _sliverHeader(String text) {
+    return Container(
+      height: 60,
+      color: Colors.lightBlue,
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      alignment: Alignment.centerLeft,
+      child: Text(text, style: const TextStyle(color: Colors.white)),
     );
   }
 }
--- lib/ui/home/home_route.dart
+++ lib/ui/home/home_route.dart
@@ -1,5 +1,5 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gallery/ui/animated_icon_route.dart';
 import 'package:flutter_gallery/ui/home/dashboard_screen.dart';
 
 class HomeRoute extends StatefulWidget {
@@ -9,28 +9,67 @@ class HomeRoute extends StatefulWidget {
   _HomeRouteState createState() => _HomeRouteState();
 }
 
-class _HomeRouteState extends State<HomeRoute> {
+class _HomeRouteState extends State<HomeRoute> with SingleTickerProviderStateMixin {
+  // Bottom navigation bar
+  late TabController _tabController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter gallery'),
       ),
-      body: const DashboardScreen(),
-      // Column(
-      //   children: [
-      //     ElevatedButton(
-      //       onPressed: () {
-      //         Navigator.push(context, MaterialPageRoute(builder: (_) => const AnimatedIconRoute()));
-      //       },
-      //       child: const Text('Animated icon route'),
-      //     ),
-      //
-      //     //   ElevatedButton(onPressed: () {
-      //     // Navigator.pushNamed(context, MaterialPageRoute(builder: (_) =>AnimatedIconRoute() )),),
-      //     // AnimatedIconRout        }, child: const Text('Animated icon'),),
-      //   ],
-      // ),
+      body: TabBarView(
+        controller: _tabController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          /// Нүүр
+          const DashboardScreen(),
+          Container(),
+          Container(),
+          Container(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _selectedIndex,
+        showElevation: true, // use this to remove appBar's elevation
+        onItemSelected: (index) => setState(() {
+          _selectedIndex = index;
+          _tabController.animateTo(index, duration: const Duration(milliseconds: 300), curve: Curves.ease);
+        }),
+        items: [
+          BottomNavyBarItem(icon: const Icon(Icons.apps), title: const Text('Home'), activeColor: Colors.red),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.people),
+            title: const Text('Users'),
+            activeColor: Colors.purpleAccent,
+          ),
+          BottomNavyBarItem(icon: const Icon(Icons.message), title: const Text('Messages'), activeColor: Colors.pink),
+          BottomNavyBarItem(icon: const Icon(Icons.settings), title: const Text('Settings'), activeColor: Colors.blue),
+        ],
+      ),
     );
   }
 }
+
+// Column(
+//   children: [
+//     ElevatedButton(
+//       onPressed: () {
+//         Navigator.push(context, MaterialPageRoute(builder: (_) => const AnimatedIconRoute()));
+//       },
+//       child: const Text('Animated icon route'),
+//     ),
+//
+//     //   ElevatedButton(onPressed: () {
+//     // Navigator.pushNamed(context, MaterialPageRoute(builder: (_) =>AnimatedIconRoute() )),),
+//     // AnimatedIconRout        }, child: const Text('Animated icon'),),
+//   ],
+// ),
